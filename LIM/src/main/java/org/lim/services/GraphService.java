23f1b7eb6c0f70ba2c7d081819f16ff2bbbd9965
A R <ar@pop-os.localdomain>
2026-03-15 21:11:26 -0400

    parser fails, looking for correct was to represent one to many in yaml file

--- LIM/src/main/java/org/lim/services/GraphService.java
+++ LIM/src/main/java/org/lim/services/GraphService.java
@@ -1,12 +1,25 @@
 package org.lim.services;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.lim.components.GraphConfig;
 import org.springframework.stereotype.Service;
+import jakarta.annotation.PostConstruct;
+
 
 @Service
 public class GraphService {
 
-    @Value("classpath:personInterest.yaml")
+    private final GraphConfig graphConfig;
+
+    public GraphService(GraphConfig graphConfig) {
+        this.graphConfig = graphConfig;
+    }  
+
+    @PostConstruct
+    public void init() {
+        System.out.println("GraphService initialized with person: " + graphConfig.getPerson());
+        //System.out.println("Interests: " + graphConfig.getInterests());
+    }
+
     public void createGraph() {
         // Logic to create graph in Neo4j using the Neo4jClient
     }
