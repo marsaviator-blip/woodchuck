58e93f619d21780bec5b9953778d004b222b4185
A R <ar@pop-os.localdomain>
2026-05-02 09:48:15 -0400

    DocumentProcessingController with swagger - not totally working

--- gaZap/backend/src/main/java/org/woodchuck/controllers/DocumentProcessingController.java
+++ gaZap/backend/src/main/java/org/woodchuck/controllers/DocumentProcessingController.java
@@ -3,14 +3,24 @@ package org.woodchuck.controllers;
 import ai.docling.serve.api.convert.request.ConvertDocumentRequest;
 import ai.docling.serve.api.convert.response.ConvertDocumentResponse;
 import ai.docling.serve.api.DoclingServeApi;
+import ai.docling.serve.api.convert.request.source.FileSource;
 import ai.docling.serve.api.convert.request.source.HttpSource;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Base64;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;    
@@ -27,7 +37,10 @@ class DocumentProcessingController {
     this.doclingServeApi = doclingServeApi;
   }
 
-  @GetMapping("/http")
+  @GetMapping("/http/{url}")
+  @Operation(summary = "Use Docling to convert a document from a given URL", 
+                description = "will provide more description soon.\n"+
+                "Cut n paste a url to a document, and Docling will convert it to a structured format.  ") 
   String convertDocumentFromHttp(@RequestParam("url") String url) {
     ConvertDocumentResponse response = doclingServeApi
       .convertSource(ConvertDocumentRequest.builder()
@@ -35,4 +48,23 @@ class DocumentProcessingController {
         .build());
       return response.getResponseType().name();
   }
+
+  @GetMapping("/file/{fullpath}")
+  @Operation(summary = "Use Docling to convert a document from a local file", 
+                description = "will provide more description soon.\n"+
+                "Upload a file, and Docling will convert it to a structured format.  \n"+
+              "temporarily hardcoded to read from classpath resource 'documents/story.pdf'") 
+  String convertDocumentFromFile(@RequestParam("fullpath") String fullpath) throws IOException {
+    Resource file = new ClassPathResource("documents/story.pdf");
+    String base64File = Base64.getEncoder().encodeToString(file.getContentAsByteArray());
+    ConvertDocumentResponse response = doclingServeApi
+      .convertSource(ConvertDocumentRequest.builder()
+        .source(FileSource.builder()
+          .filename("story.pdf")
+          .base64String(base64File)
+          .build())
+        .build());
+    return response.getResponseType().name();
+  }
+
 }
